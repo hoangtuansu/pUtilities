70c46a501e829be5941860a4ab3ed5b6f9f6987b
hoangtuansu <hoang.su.tk@gmail.com>
2018-05-08 14:37:47 -0400

    Very basic socket communication

---
+++ socket/delay measurement/delay_socket_client.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+import datetime as dt
+import time
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (sys.argv[1], 8089)
+client_socket.connect(server_address)
+count = 1
+
+try:
+    while count < 10:
+        t1 = dt.datetime.now()
+        client_socket.send('hello')
+        data = client_socket.recv(1024)
+        t2 = dt.datetime.now()
+        delay = (t2.microsecond - t1.microsecond)/1e6
+        print("from server: " + data + ", delay: " + str(delay))
+        count=count + 1
+        time.sleep(1)
+finally:
+    client_socket.close()
---
+++ socket/delay measurement/delay_socket_server.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (sys.argv[1], 8089)
+server_socket.bind(server_address)
+server_socket.listen(5) # become a server socket, maximum 5 connections
+
+while True:
+    connection, address = server_socket.accept()
+    try:
+        while True:
+            data = connection.recv(64)
+            print("receive: " + data)
+            if data:
+                connection.send("got: " + data)
+            else:
+                break
+    finally:
+        connection.close()
+
