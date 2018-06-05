4e807342ae6feefcc58275890dc02fbcba1623bf
hoangtuansu <hoang.su.tk@gmail.com>
2018-06-05 14:16:50 -0400

    - Allow to run multiple shell commands

--- command_execute.py
+++ command_execute.py
@@ -2,9 +2,15 @@ import subprocess
 import shlex
 
 def run(cmd):
-	process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
-	out, err = process.communicate()
-	return out.decode()
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    return out.decode()
+
+def run_multiples(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out,err = process.communicate()
+    return out.decode()
 
 if __name__ == '__main__':
-	print run('dir')
+    print run('dir')
+    print run_multiples('echo a;echo b')
