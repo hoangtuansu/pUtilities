2c702566fcab8097314e23f3db026ca39d236172
hoangtuansu <hoang.su.tk@gmail.com>
2018-06-15 11:50:31 -0400

    - Fix delay in miliseconds

--- socket/delay measurement/delay_socket_client.py
+++ socket/delay measurement/delay_socket_client.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
             client_socket.send('hello')
             data = client_socket.recv(1024)
             t2 = dt.datetime.now()
-            delay = (t2.microsecond - t1.microsecond)/1e6
+            delay = (t2 - t1).microseconds/1e6
             print("from server: " + data + ", delay: " + str(delay))
             count=count + 1
             time.sleep(1)
