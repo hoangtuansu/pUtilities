ac3983345389c7fed00e656d664dcb28eba8483a
hoangtuansu <hoang.su.tk@gmail.com>
2018-11-15 16:01:20 -0500

    - Multi-threading usage

--- show_graph.py
+++ show_graph.py
@@ -1,11 +1,4 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
-plt.axis([0, 10, 0, 1])
-
-for i in range(10):
-    y = np.random.random()
-    plt.scatter(i, y)
-    plt.pause(0.05)
-
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
 plt.show()
--- write_to_file.py
+++ write_to_file.py
@@ -1,3 +1,10 @@
 file = open("data.txt", "w")
 file.write("Hello World")
 file.close()
+
+#write an array to a file
+a = [1,2,3]
+file = open("data.txt", "w")
+for i in a:
+    file.write(str(i) + ' ')
+file.close()
